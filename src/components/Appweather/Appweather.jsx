ebb2b871185875a988296b0a3ce6699920900dff
Yassine MGHAZLI <yassinemghazli03@gmail.com>
2023-01-15 18:14:52 +0100

    Commit

---
+++ src/components/Appweather/Appweather.jsx
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from "react";
+import Journey from "../Journey/Journey";
+export default function Appweather() {
+  const [search, setSearch] = useState("");
+  const [update, setUpdate] = useState(false);
+  const [data, setData] = useState([]);
+  const [dayArray, setDayArray] = useState([]);
+  const [city, setCity] = useState("");
+  const [temp, setTemp] = useState("");
+  const handleInput = (e) => {
+    setSearch(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (search.trim() !== "") {
+      setUpdate(true);
+      fetch(
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${search}/next7days?unitGroup=uk&key=VJ3DZUDUFNBGMJT5A8CCQNU3B&contentType=json`
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          setData(result);
+        });
+    }
+  };
+
+  //   console.log();
+  // data.currentConditions.temp
+  useEffect(() => {
+    if (update) {
+      setDayArray(data.days);
+      setCity(data.address);
+      setTemp(data.currentConditions.temp);
+    }
+    setSearch("");
+  }, [data]);
+  //   console.log(search);
+  return (
+    <div className="">
+      <h1 className="text-center md:text-6xl text-2xl tracking-wide font-semibold text-sky-300">
+        Weather App
+      </h1>
+      <h2
+        className={
+          temp < 0
+            ? "text-cyan-600 font-bold text-9xl fixed"
+            : temp > 20
+            ? "text-red-400 font-extrabold text-xl fixed"
+            : "text-sky-200 font-extrabold text-9xl fixed"
+        }
+      >
+        {temp + "°C"}
+      </h2>
+      <form onSubmit={handleSubmit} className="flex mb-20 justify-center mt-20">
+        <input
+          className="px-5 py-2 text-lg outline-none tracking-wider md:w-3/12 w-3/6"
+          type="text"
+          placeholder="Enter city..."
+          onChange={handleInput}
+          value={search}
+        />
+        <button
+          className="px-5 bg-teal-300 rounded-r-lg text-white font-bold text-lg shadow-lg shadow-cyan-500/50"
+          type="submit"
+        >
+          Search
+        </button>
+      </form>
+      <h1 className="text-center font-bold p-10 text-white md:text-6xl text-2xl">
+        {city}
+      </h1>
+      <div className="flex bg-black/25 justify-center flex-wrap">
+        {dayArray.map((dayItem, index) => {
+          return (
+            <div className="text-center w-screen md:w-1/4 lg:w-1/5 border-2 my-6 mx-5 p-10 border-solid border-sky-200 rounded-lg">
+              <Journey
+                dayDate={dayArray[index].datetime}
+                temperature={dayArray[index].temp}
+                srcIcon={`icons/${dayArray[index].icon}.png`}
+                description={dayArray[index].description}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Journey/Journey.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default function Journey(props) {
+  return (
+    <div className="">
+      <div className="day-container">
+        <div className="city-date">
+          <h1 className="text-2xl">{props.city}</h1>
+          <h2 className="text-2xl font-semibold text-sky-200">
+            {props.dayDate}
+          </h2>
+        </div>
+
+        <div className="previsions ">
+          <div className="flex justify-center ">
+            <img
+              src={props.srcIcon}
+              className="m-0 "
+              onError={(event) => {
+                event.target.src = "icons/cloudy.png";
+              }}
+            ></img>
+          </div>
+
+          <h3
+            className={
+              props.temperature < 0
+                ? "text-cyan-600 font-extrabold text-lg"
+                : props.temperature > 20
+                ? "text-red-400 font-extrabold text-lg"
+                : "text-sky-200 font-extrabold text-lg"
+            }
+          >
+            {props.temperature}°C
+          </h3>
+          <p className="text-sky-200 ">{props.description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
